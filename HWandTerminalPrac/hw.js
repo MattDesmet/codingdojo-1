4472f886d1f572d8c465366b5da4a75c54d4f206
Leah Rimington <leah@mycodebuilder.com>
2016-12-15 20:11:43 -0800

    recommitting old files and uploading new assignments with better organization

---
+++ HWandTerminalPrac/hw.js
@@ -0,0 +1,19 @@
+var num=6;
+
+while (num < 60001) {
+  console.log(num);
+    num+=6;
+}
+
+// press up arrow on keyboard in terminal = previous command
+// press down arrow = next command
+// right and left arrows edit current line
+// !! = repeat last command
+// history = show recent commands
+// !123 = repeat command #123
+// command-k = clear terminal window
+// control-c = kill current process
+// control-d = tell current process there is no more input
+//
+// node myfine.js = run myfile.js using the terminal
+// node myfile.js | more = run myfile.js and look at the result page at a page at a time
